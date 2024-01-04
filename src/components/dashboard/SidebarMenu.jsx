3d0e6a7ecd176b6049cc846505c06e93b41a185a
subrata2402 <subratadas3250@gmail.com>
2024-01-04 22:17:18 +0530

    add border to submission title

--- src/components/dashboard/SidebarMenu.jsx
+++ src/components/dashboard/SidebarMenu.jsx
@@ -6,33 +6,33 @@ import styles from "./sidebarStyle.module.css";
 import { NavLink } from "react-router-dom";
 
 function SidebarMenu() {
-  return (
-    <ul>
-      <li id="sidebar-link">
-        <NavLink to="/dashboard/view-submission" className="btn text-white text-center">
-          <GrView className={`${styles.dashboardIconView} text-light`} />
-          View Submission
-        </NavLink>
-      </li>
-      <li id="sidebar-link">
-        <NavLink 
-        to="/dashboard/add-submission"
-        className="btn text-white text-center">
-          <BiMessageSquareAdd className={`${styles.dashboardIconView}`} />
-          Add Submission
-        </NavLink>
-      </li>
-      <li id="sidebar-link">
-        <NavLink
-          to="/dashboard/analytical-report"
-          className="btn text-white text-center"
-        >
-          <MdOutlineAnalytics className={`${styles.dashboardIconView}`} />
-          Analytical Report
-        </NavLink>
-      </li>
-    </ul>
-  );
+    return (
+        <ul>
+            <li id="sidebar-link">
+                <NavLink to="/dashboard/view-submission" className="btn text-white text-center">
+                    <GrView className={`${styles.dashboardIconView}`} />
+                    View Submission
+                </NavLink>
+            </li>
+            <li id="sidebar-link">
+                <NavLink
+                    to="/dashboard/add-submission"
+                    className="btn text-white text-center">
+                    <BiMessageSquareAdd className={`${styles.dashboardIconView}`} />
+                    Add Submission
+                </NavLink>
+            </li>
+            <li id="sidebar-link">
+                <NavLink
+                    to="/dashboard/analytical-report"
+                    className="btn text-white text-center"
+                >
+                    <MdOutlineAnalytics className={`${styles.dashboardIconView}`} />
+                    Analytical Report
+                </NavLink>
+            </li>
+        </ul>
+    );
 }
 
 export default SidebarMenu;
