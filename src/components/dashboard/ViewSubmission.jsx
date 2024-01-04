1c5263bc07718aad792dd227a3891b6b51913bd3
subrata2402 <subratadas3250@gmail.com>
2024-01-04 22:08:40 +0530

    update login, .remember-forgot

--- src/components/dashboard/ViewSubmission.jsx
+++ src/components/dashboard/ViewSubmission.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function ViewSubmission() {
-  return (
-    <div>
-      <h2>coming soon ... </h2>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Coming soon ... </h1>
+        </div>
+    );
 }
 
 export default ViewSubmission
