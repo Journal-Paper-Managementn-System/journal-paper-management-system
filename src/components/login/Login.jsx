6f0cb4e9902546e66de07f4360512a20eba3f3c8
Supriyo95 <supriyo2001dolui@gmail.com>
2024-01-04 14:07:56 +0530

    adding password visible & invisible features

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { GrView } from "react-icons/gr";
+import { FaUser } from "react-icons/fa";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
 
 function Login() {
+  const [visibleIcon,setVisibleIcon] = useState(true);
+  const pswd = useRef();
+
+  const handleOnVisible = ()=>{
+    if(pswd.current.type !== 'password'){
+      pswd.current.type = 'password';
+      setVisibleIcon(true)
+    }
+    else{
+      pswd.current.type = 'text';
+      setVisibleIcon(!visibleIcon);
+    }
+  }
+
   return (
     <section className="wrapper">
       <div className="center-wrapper">
         <form action="/">
-          <h1>Login</h1>
+          <h1>Login {visibleIcon}</h1>
           <div className="input-box">
             <label htmlFor="username">Username</label>
             <input
@@ -17,11 +33,7 @@ function Login() {
               placeholder="enter your valid username"
               required
             />
-            <box-icon
-              type="solid"
-              name="user"
-              className="input-icon"
-            ></box-icon>
+            <span className="input-icon"> <FaUser className="user-icon"/> </span>
           </div>
           <div className="input-box ">
             <label htmlFor="password">Password</label>
@@ -29,11 +41,17 @@ function Login() {
               type="password"
               name="password"
               id="password"
+              ref={pswd}
               placeholder="enter your valid password"
               required
             />
             <span className="input-icon">
-              <GrView />
+              {
+                visibleIcon ? 
+                <GrView onClick={handleOnVisible} className="pswd-icon"/> 
+                : <AiOutlineEyeInvisible
+                 onClick={handleOnVisible} className="pswd-icon" fontSize={24}/>
+              }
             </span>
           </div>
           <div className="remember-forgot">
